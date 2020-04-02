f97501ead357185cda33be2843f17913309d9539
Koichi Nakashima <koichi@nksm.name>
2020-04-03 02:09:42 +0900

    Update to 0.23.0

--- shellspec.rb
+++ shellspec.rb
@@ -1,8 +1,8 @@
 class Shellspec < Formula
   desc "BDD style unit testing framework for POSIX compliant shell script"
   homepage "https://shellspec.info"
-  url "https://github.com/shellspec/shellspec/archive/0.22.0.tar.gz"
-  sha256 "a26087ef81eaed64990d40aa06f018916690b7dad9720998a440822645eae688"
+  url "https://github.com/shellspec/shellspec/archive/0.23.0.tar.gz"
+  sha256 "96b89b2a738c2edaae949b9c2fc01c998f00223db405c8c006c279f122092471"
 
   bottle :unneeded
 
