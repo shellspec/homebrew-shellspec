f8025a735a0ecba2f19cd7a1375257b511839ca2
Koichi Nakashima <koichi@nksm.name>
2020-07-13 01:34:38 +0900

    Update description

--- shellspec.rb
+++ shellspec.rb
@@ -1,5 +1,5 @@
 class Shellspec < Formula
-  desc "BDD style unit testing framework for POSIX compliant shell script"
+  desc "A full-featured BDD unit testing framework for dash, bash, ksh, zsh and all POSIX shells"
   homepage "https://shellspec.info"
   url "https://github.com/shellspec/shellspec/archive/0.26.0.tar.gz"
   sha256 "67d7ddf18157be611e72357f527efc84d72135ab2a68dd24ba20ed2fe3443e9c"
