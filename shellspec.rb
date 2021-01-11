f5f36a1b82c466917dc6e16594501be65422fcec
Koichi Nakashima <koichi@nksm.name>
2021-01-11 15:01:22 +0900

    Update to 0.28.1

--- shellspec.rb
+++ shellspec.rb
@@ -1,8 +1,8 @@
 class Shellspec < Formula
   desc "A full-featured BDD unit testing framework for dash, bash, ksh, zsh and all POSIX shells"
   homepage "https://shellspec.info"
-  url "https://github.com/shellspec/shellspec/archive/0.28.0.tar.gz"
-  sha256 "9bb444dafcc54456206f887a3979e912ef4c966d4df4862c09bf2ccb5310bdb4"
+  url "https://github.com/shellspec/shellspec/archive/0.28.1.tar.gz"
+  sha256 "400d835466429a5fe6c77a62775a9173729d61dd43e05dfa893e8cf6cb511783"
 
   bottle :unneeded
 
